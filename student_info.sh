8c25e0cc10f4fa6547ce763c8e88bd039524d822
Daniel Polukchi <danilabodrov746@gmail.com>
2025-01-25 04:47:33 +0000

    Save progress: 2420.1

--- student_info.sh
+++ student_info.sh
@@ -34,3 +34,4 @@ echo -e "\nList of majors, in alphabetical order, that either no student is taki
 echo "$($PSQL "SELECT major FROM students FULL JOIN majors ON students.major_id = majors.major_id WHERE major IS NOT NULL AND (student_id IS NULL OR first_name ILIKE '%ma%') ORDER BY major")"
 
 echo -e "\nList of unique courses, in reverse alphabetical order, that no student or 'Obie Hilpert' is taking:"
+echo "$($PSQL "SELECT DISTINCT(course) FROM courses FULL JOIN majors_courses  USING(course_id) FULL JOIN students USING(major_id) WHERE student_id IS NULL OR first_name = 'Obie' ORDER BY course DESC;")"
